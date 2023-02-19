c3ddee4269488375f60564c59dd1c7df457db4da
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-02-19 07:12:37 +0530

    Variable Creation In JavaScript

---
+++ JavaScript/Basic_Programs/01_Variables.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+    <script>
+        var No1 = 10;
+        var No2 = 25;
+
+        var Sum = No1 + No2;
+
+        console.log(Sum);
+        window.alert('Welcome To Variables');
+    </script>
+</head>
+<body>
+    <h2>Variables</h2>
+</body>
+</html>
