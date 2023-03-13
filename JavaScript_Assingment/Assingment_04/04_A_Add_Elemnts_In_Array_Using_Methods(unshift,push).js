8afdcf8a5b657e8fa8855e6370ae0e9e062998fb
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-03-13 20:48:23 +0530

    Add Element In Array Using Methods

---
+++ JavaScript_Assingment/Assingment_04/04_A_Add_Elemnts_In_Array_Using_Methods(unshift,push).js
@@ -0,0 +1,6 @@
+const Vegitables = ["Cucumber","Pumpkin","Tomato"];
+
+Vegitables.push("Corn","Green Peas","String Beans");
+Vegitables.unshift("Patato","Onion","Peppers");
+
+console.log(Vegitables);
