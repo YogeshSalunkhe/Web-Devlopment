ca5d312a1a8c125c6c6c0b664c761464c6965309
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-03-13 20:49:44 +0530

    Print Given Output Using Splice

---
+++ JavaScript_Assingment/Assingment_04/04_C_Insert_And_Delete_Array_Element_Using_(splice())_Method.js
@@ -0,0 +1,7 @@
+const Vegitables = ["Patato","Onion","Peppers","Cucumber","Pumpkin","Tomato","Corn","Green Peas","String Beans"];
+
+Vegitables.splice(1,0,"Brinjal");
+console.log(Vegitables);
+
+Vegitables.splice(4,1,"Carrot");
+console.log(Vegitables);
