892e5403e6321bb25d5597ca402a9f6b843053c5
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-03-13 20:49:10 +0530

    Print Given Output Using Slice

---
+++ JavaScript_Assingment/Assingment_04/04_B_Print_Given_Array_Using_(slice())_Method.js
@@ -0,0 +1,7 @@
+const Vegitables = ["Patato","Onion","Peppers","Cucumber","Pumpkin","Tomato","Corn","Green Peas","String Beans"];
+
+console.log(Vegitables.slice(3,6));
+console.log(Vegitables.slice(7,9));
+
+
+console.log(Vegitables.slice(Vegitables.slice(3,5),Vegitables.slice(8,9)));
