eb733454be5937e093b3604de70b1bec9c82b60b
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-03-13 20:47:00 +0530

    Replace Array Elemnt

---
+++ JavaScript_Assingment/Assingment_04/03_Replace_Element_Of_Array_In_Given_Order.js
@@ -0,0 +1,8 @@
+let StrOne = "good development"; 
+let StrTwo = "Hello All";
+let StrThree = "learn";
+
+StrOne = StrOne.replace("good","web");
+StrTwo = StrTwo.replace("All","WEBx86_Students");
+StrThree = StrTwo.concat(" ",StrThree," ",StrOne);
+console.log(StrThree);
